0c8d4e9b817865ba24d4f3f4dd5e00b85962f746
Christian Joy Magnase <magnasechristianjoy@gmail.com>
2025-05-05 02:21:33 +0800

    action buttons

---
+++ Pages/Supplier/Delete.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using IT15_Final_Proj.Models;
+using IT15_Final_Proj.Services;
+
+namespace IT15_Final_Proj.Pages.Supplier
+{
+    public class DeleteProductModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public DeleteProductModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Product = await _context.Products.FindAsync(id);
+            if (Product == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var productToDelete = await _context.Products.FindAsync(Product.Id);
+            if (productToDelete == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(productToDelete);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Supplier/Products");
+        }
+    }
+}
